01c68367586b2bdab1a6e646ac9e4a38eb4bb150
ApoorvVerma98 <apoorv.verma9@gmail.com>
2023-03-07 00:20:31 +0530

    assignmentday2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component1 } from "./fourComponent";
+import { Component2 } from "./fourComponent";
+import { Component3 } from "./fourComponent";
+import { Component4 } from "./fourComponent";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+
+  <div className="App">
+{/* {<h1>I am heading one ...from first component</h1> 
+<h2> I am heading two .....from second component</h2>
+<h3> I am heading two .....from second component</h3>
+<button>Click Me</button>} */}
+
+
+<Component1/>
+<Component2/>
+<Component3/>
+<Component4/>
+</div>
+
+
+);
 }
 
 export default App;
